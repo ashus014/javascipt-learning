310a9ce6d0a04cdd682bb1bb534cc0812f1b841f
Ashu Singh <ashu.ask.singh@gmail.com>
2022-11-17 00:03:39 +0530

    default parameters in ES6

--- workingWithFunctions.js
+++ workingWithFunctions.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("Hello World");
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 33) {
+  // Below is the ES5 way for short circuiting
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 33;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("Indigo 6E134");
